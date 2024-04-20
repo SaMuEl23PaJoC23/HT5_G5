ec190da3479b3eaca9b87eb78b45ad283c604566
Samuel Alejandro Pajoc Raymundo <sam.alejandro.sam@gmail.com>
2024-04-20 00:11:24 -0600

    Actualizacion final
    Completo

--- codigoSerial.py
+++ codigoSerial.py
@@ -2,3 +2,14 @@ import serial, time
 
 puertoSerie = serial.Serial('COM3', 9600)
 time.sleep(2)
+
+data=''
+while (data != '3'):
+    data = input("Introducir un valor (1 o 0): ")
+    if(data == '1'):
+        puertoSerie.write(b'1')
+        print("LED encendido !! \n")
+    elif(data == '0'):
+        puertoSerie.write(b'0')
+        print("LED apagado... \n")
+puertoSerie.close()
